6eba8b8798e5dcc7be528c01a14244818f39a7b7
wesinator <5124946+wesinator@users.noreply.github.com>
2018-06-02 23:56:09 -0400

    Remove fopen mixed modes
    Fixes #2

--- lib/skulfs.c
+++ lib/skulfs.c
@@ -86,7 +86,7 @@ int read_header(pheader *header, char *path, int *slot){
 	FILE *disk;
 	int i;
 
-	if(!(disk=fopen(path,"rb+"))){
+	if(!(disk=fopen(path,"rb"))){
 		perror("fopen");
 		return 0;
 	}
@@ -225,7 +225,7 @@ int read_disk(unsigned char *dst, size_t size, char *path, size_t offset){
 	
 	FILE *file;
 
-	if(!(file=fopen(path,"rb+"))){
+	if(!(file=fopen(path,"rb"))){
 		perror("fopen");
 		return 0;
 	}
